3c41d5b977ef3946f99cab644c38163787c2bde3
Theodore <theodore.aaron-obelley@nextdlp.com>
2025-03-07 19:46:50 +0000

    emacs revamp

--- apps/config/emacs/init.el
+++ apps/config/emacs/init.el
@@ -1,4 +1,4 @@
-
+;; -------------------------------------------------------------------
 ;; Package Manager
 ;; -------------------------------------------------------------------
 
@@ -19,6 +19,102 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
+;; use-package using straight-use-package
+(straight-use-package 'use-package)
+
+;; set use-package as default
+(use-package straight
+  :custom
+  (straight-use-package-by-default t))
+
+;; -------------------------------------------------------------------
+;; Auto update packages
+;; -------------------------------------------------------------------
+
+(use-package auto-package-update
+  :init
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe))
+
+;; -------------------------------------------------------------------
+;; Language Server Protocol
+;; -------------------------------------------------------------------
+
+;; Configuration -----------------
+(use-package lsp-mode
+  :init (setq lsp-keymap-prefix "C-l")
+  :hook (
+	 ;; Python
+	 (python-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; UI Mode
+(use-package lsp-ui :commands lsp-ui-mode)
+
+;; helm symbol
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+
+(use-package which-key
+    :config
+    (which-key-mode))
+
+;; -------------------------------------------------------------------
+;; Packages
+;; -------------------------------------------------------------------
+
+;; -------------------- General: Better keybinding management
+(use-package general)
+
+;; -------------------- Projectile
+(use-package projectile
+  :ensure t
+  :init
+  (setq projectile-cleanup-known-projects t)
+  (setq projectile-auto-discover t)
+  :bind (("C-p" . projectile-command-map))
+  :config
+  (projectile-mode +1))
+
+(use-package helm-projectile
+  :config
+  (helm-projectile-on))
+
+;; -------------------- hydra
+(use-package hydra)
+
+;; -------------------- pyenv
+(use-package pyvenv
+  :ensure t
+  :config
+  (pyvenv-mode 1))
+
+;; -------------------- Sublimity
+(use-package sublimity
+  :config
+  (sublimity-mode 1))
+
+;; -------------------- Dump Jump
+(use-package dumb-jump
+  :after
+  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
+
+;; -------------------- Helm: Improved buffer & search packages
+(use-package helm
+  :config
+  (helm-mode 1)
+  :bind ("C-o" . 'helm-find-files))
+
+;; -------------------- swiper-helm
+(use-package swiper-helm
+  :bind (("C-f" . swiper-helm)
+	 :map swiper-helm-keymap
+	 ("<tab>" . helm-next-line)
+	 ("<backtab>" . helm-previous-line)))
+
 ;; -------------------------------------------------------------------
 ;; Keymap
 ;; -------------------------------------------------------------------
@@ -27,33 +123,27 @@
 (cua-mode)
 (setq select-enable-clipboard t)
 
-;; Exit Emacs
-(keymap-global-unset "C-q")
-(keymap-global-set "C-q" 'save-buffers-kill-terminal)
+(general-define-key
+  "C-q" 'save-buffers-kill-terminal ; exit emacs
+  "C-s" 'save-buffer ; save current screen
 
-;; Save current screen
-(keymap-global-set "C-s" 'save-buffer)
-
-;; Opening files and folders
-(keymap-global-set "C-o" 'find-file)
-
-;; Searching within window
-(keymap-global-set "C-f" 'isearch-forward)
+  ;; Swapping lines
+  "M-<up>" '(lambda () (interactive) (transpose-lines -1))
+  "M-<down>" 'transpose-lines)
 
 ;; Moving between windows
-(keymap-global-set "C-x <up>" 'windmove-up)
-(keymap-global-set "C-x <down>" 'windmove-down)
-(keymap-global-set "C-x <left>" 'windmove-left)
-(keymap-global-set "C-x <right>" 'windmove-right)
+(general-define-key
+  :prefix "C-x"
+  "<up>"    'windmove-up
+  "<down>"  'windmove-down
+  "<left>"  'windmove-left
+  "<right>" 'windmove-right)
 
-
-;; Swapping lines
-(keymap-global-set "M-<up>"
-		   (lambda ()
-		     (interactive)
-		     (transpose-lines -1)))
-
-(keymap-global-set "M-<down>" 'transpose-lines)
+;; Moving around text
+(general-define-key
+  "C-<right>" '(lambda () (interactive) (forward-same-syntax 1))
+  "C-<left>"  '(lambda () (interactive) (forward-same-syntax -1))
+ )
 
 ;; -------------------------------------------------------------------
 ;; Accessiblity
@@ -62,18 +152,22 @@
 ;; Enable mouse mode
 (xterm-mouse-mode)
 
-;; Helm: Improved buffer & search packages
-(straight-use-package 'helm)
-(helm-mode 1)
-
 ;; -------------------------------------------------------------------
 ;; Themes
 ;; -------------------------------------------------------------------
 
-(straight-use-package 'catppuccin-theme)
+(use-package catppuccin-theme
+  :init (setq catppuccin-flavor 'latte)
+  :config
+  (load-theme 'catppuccin :no-confirm)
+  (catppuccin-reload))
 
-;; Catppuccin theme
-(load-theme 'catppuccin :no-confirm)
+;; -------------------------------------------------------------------
+;; Projects
+;; -------------------------------------------------------------------
 
-(setq catppuccin-flavor 'latte)
-(catppuccin-reload)
+(dir-locals-set-class-variables 'agent-performance
+   '((nil . ((pyvenv-activate . "~/go/src/repo.jazzdev.io/jazz/agent-performance/.venv")))))
+
+(dir-locals-set-directory-class
+   "~/go/src/repo.jazzdev.io/jazz/agent-performance" 'agent-performance)
