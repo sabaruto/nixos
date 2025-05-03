a084071dc20818d4bf12ec6f360babc489468c4f
sabaruto <theodoreaaronobelley@hotmail.co.uk>
2025-05-03 12:43:25 +0100

    update zalu

--- pcs/zalu/configuration.nix
+++ pcs/zalu/configuration.nix
@@ -15,18 +15,14 @@
     swapSize = 16 * 1024;
     gpu = "none";
 
-    # system.kernel = {
-    #   enable = true;
-    #   version = "6.14.0";
-    #   altVersion = "6.14";
-    #   url = "https://github.com/torvalds/linux/archive/refs/tags/v6.14.tar.gz";
-    #   hash = "sha256-fXlg1s4nd0Ppbb2NlPzpPxN5mmm9mj6YQe6//eSr5sI=";
-    # };
-
     desktopEnvironment = "gnome";
+
+    shells.zsh.enable = true;
 
     apps = {
       steam.enable = true;
     };
   };
+
+  users.defaultUserShell = pkgs.zsh;
 }
