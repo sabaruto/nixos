13ade5b401e4ad61dc0824f3bd27050cc645d2cb
sabaruto <theodoreaaronobelley@hotmail.co.uk>
2025-05-23 09:15:44 +0100

    missed files

---
+++ modules/home-manager/default/apps/neovim/lua/plugins/barbecue.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons",
+		},
+
+		opts = {
+			context_follow_icon_color = true,
+			show_modified = true,
+		},
+	}
+}
