41c4bb37e4cc5c2514e2b808d25c585ec67bb273
Theodosia Kalu <theodosiakalu@gmail.com>
2025-06-07 19:38:46 +0100

    add oh-my-zsh files

--- nix/modules/dotfiles/default.nix
+++ nix/modules/dotfiles/default.nix
@@ -1,18 +1,31 @@
 { lib, config, pkgs, ... }:
 with lib;
-let cfg = config.localModules;
+let
+  cfg = config.localModules;
+  dotfilesDirectory =
+    "${config.home.homeDirectory}/github.com/sabaruto/nixos/nix/modules/dotfiles";
 in {
   options.localModules = {
 
   };
 
   config = {
-    home.file."nvim" = mkIf cfg.apps.neovim.enable {
-      enable = true;
-      recursive = true;
-      source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/github.com/sabaruto/nixos/nix/modules/dotfiles/nvim";
-      target = ".config/nvim";
+    home.file = {
+      "nvim" = mkIf cfg.apps.neovim.enable {
+        enable = true;
+        recursive = true;
+        source =
+          config.lib.file.mkOutOfStoreSymlink "${dotfilesDirectory}/nvim";
+        target = ".config/nvim";
+      };
+
+      ".oh-my-zsh" = mkIf cfg.shells.zsh.enable {
+        enable = true;
+        recursive = true;
+        source =
+          config.lib.file.mkOutOfStoreSymlink "${dotfilesDirectory}/.oh-my-zsh";
+        target = ".oh-my-zsh";
+      };
     };
   };
 }
