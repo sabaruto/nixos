bbb20c81e68ddee202c367005f5d4f20d92876a5
Theodosia Kalu <theodosiakalu@gmail.com>
2025-06-15 20:14:50 +0100

    update

--- modules/system/base/default.nix
+++ modules/system/base/default.nix
@@ -175,12 +175,19 @@ in {
     console.keyMap = "uk";
 
     security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
+    services = {
+      syncthing = {
+        enable = true;
+        openDefaultPorts = true;
+      };
+
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+      };
     };
 
     # Allow unfree packages
