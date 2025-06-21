b72b421daf92a586f7ab53f72b1b6c2b6fe2db59
Theodosia Kalu <theodosiakalu@gmail.com>
2025-06-21 14:12:07 +0100

    use CTRL

--- modules/dotfiles/.wezterm.lua
+++ modules/dotfiles/.wezterm.lua
@@ -3,15 +3,16 @@ local act = wezterm.action
 
 local config = wezterm.config_builder()
 
-config.font_size = 12
-config.color_scheme = 'Rosé Pine Moon (Gogh)'
--- config.color_scheme = "Rosé Pine Dawn (Gogh)"
+config.font_size = 14
+-- config.color_scheme = 'Rosé Pine Moon (Gogh)'
+config.color_scheme = "Rosé Pine Dawn (Gogh)"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.window_decorations = "NONE"
+config.hide_tab_bar_if_only_one_tab = true
 
 config.leader = {
 	key = " ",
-	mods = "CTRL|ALT",
+	mods = "CTRL",
 	timeout_milliseconds = 1000,
 }
 
